412612df8ad16e67fc0fe6660f23129de4bca52c
Erfan Ebrahimkhani <se.erfanebrahimkhani2@gmail.com>
2025-02-26 18:13:50 +0330

    ساخت کامپوننت های مسیریابی

--- frontend/react-ts/src/main.tsx
+++ frontend/react-ts/src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from './App';
+import Home from './pages/Home';
+import Status from './pages/Status';
+import Analytics from './pages/Analytics';
+import Sensor from './pages/Sensor';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<Home />} />
+        <Route path="status" element={<Status />} />
+        <Route path="analytics" element={<Analytics />} />
+        <Route path="sensor" element={<Sensor />} />
+      </Route>
+    </Routes>
+  </Router>
+);
