5a101096ffb8c09ff25b7f60c85675a6afeac137
Ali <alyssa18891889@gmail.com>
2025-02-28 18:55:18 +0330

    بروزرسانی2

--- frontend/react-ts/src/pages/Home.tsx
+++ frontend/react-ts/src/pages/Home.tsx
@@ -1,5 +1,8 @@
   
 import PageMeta from "../components/common/PageMeta";
+import EcommerceMetrics from "../components/ecommerce/EcommerceMetrics";
+  
+import StatisticsChart from "../components/ecommerce/StatisticsChart";
 
 export default function Home() {
   return (
@@ -8,25 +11,17 @@ export default function Home() {
         title="Home"
         description="This is Home page"
       />
-      <div className="grid grid-cols-12 gap-4 md:gap-6">
-        <div className="border col-span-12 space-y-6 xl:col-span-7">
-a
+      <div className="grid grid-cols-12 gap-4 md:gap-4">
+      <div className="col-span-12 space-y-6 xl:col-span-7">
+      <EcommerceMetrics />
+ 
+ 
         </div>
-
-        <div className="border col-span-12 xl:col-span-5">
-          b
-        </div>
+ 
 
         <div className="border col-span-12">
-         c
-        </div>
+        <StatisticsChart />
 
-        <div className="border col-span-12 xl:col-span-5">
-   d
-        </div>
-
-        <div className="border col-span-12 xl:col-span-7">
-          e
         </div>
       </div>
     </>
