143330039d542eff7213b2d6d2d8437fccb134c8
Erfan Ebrahimkhani <se.erfanebrahimkhani2@gmail.com>
2025-03-02 16:40:37 +0330

    آپلود مجدد فولدر فرانت با جزییات 3 بعدی

--- frontend/react-ts/src/pages/Home.tsx
+++ frontend/react-ts/src/pages/Home.tsx
@@ -3,7 +3,7 @@ import PageMeta from "../components/common/PageMeta";
 import EcommerceMetrics from "../components/ecommerce/EcommerceMetrics";
   
 import StatisticsChart from "../components/ecommerce/StatisticsChart";
-
+ 
 export default function Home() {
   return (
     <>
@@ -11,11 +11,11 @@ export default function Home() {
         title="Home"
         description="This is Home page"
       />
-      <div className="grid grid-cols-12 gap-4 md:gap-4">
+      <div className="grid grid-cols-6 gap-4 md:gap-3">
       <div className="col-span-12 space-y-6 xl:col-span-7">
       <EcommerceMetrics />
  
- 
+      
         </div>
  
 
